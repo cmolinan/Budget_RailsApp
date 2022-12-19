da2ec949f84cd80e7bf7b4c7e828222b433a9571
cmolinan <cmolinan@gmail.com>
2022-12-19 17:21:15 -0500

    Rails  models associations and generate migrate files

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,7 @@
 class Group < ApplicationRecord
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :entities, dependent: :destroy
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
--- db/migrate/20221219215828_create_entities.rb
+++ db/migrate/20221219215828_create_entities.rb
@@ -2,7 +2,7 @@ class CreateEntities < ActiveRecord::Migration[7.0]
   def change
     create_table :entities do |t|
       t.string :name
-      t.float :amount
+      t.float :amount, default: 0.0
 
       t.timestamps
     end
