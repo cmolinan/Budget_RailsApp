793f9060a3a65a1512c4bfe86ea7a8eabe0a8252
cmolinan <cmolinan@gmail.com>
2022-12-19 17:32:06 -0500

    Rails generate controllers and action views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'groups/index'
+  get 'groups/new'
+  get 'entities/index'
+  get 'entities/new'
+  get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
