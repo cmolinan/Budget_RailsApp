245b65b10517c17c62fd7254c9d887945a8e9571
cmolinan <cmolinan@gmail.com>
2022-12-19 18:02:18 -0500

    Basic install and configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'groups/index'
   get 'groups/new'
   get 'entities/index'
@@ -7,5 +8,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
