25a362ebd0b48fea2cad1133bc7044bd9ed0e04e
cmolinan <cmolinan@gmail.com>
2022-12-21 01:34:40 -0500

    splash, navbar, login and registration screens with styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
 
+  # Global var for define Title of Nav Bar
+  $context = ''
+
+  def after_sign_in_path_for(_resource)
+    groups_path
+  end
+
   # def after_sign_in_path_for(resource_or_scope)
   #   if resource_or_scope.is_a?(User)
   #     users_url
