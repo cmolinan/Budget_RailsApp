3287cdd7358c9cfcbef74111b7002ddb8c03a0fd
cmolinan <cmolinan@gmail.com>
2022-12-23 18:55:15 -0500

    update sh file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,6 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rake db:truncate_all
+bundle exec rake db:seed
 npm install
