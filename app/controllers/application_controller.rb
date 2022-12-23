b154442553540ceb104049fe08a847bc493e62d5
cmolinan <cmolinan@gmail.com>
2022-12-23 12:47:48 -0500

    join navbar.css with application.css file. And others improves

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
 
   # Global var for define Title of Nav Bar
@@ -9,19 +8,6 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
     groups_path
   end
-
-  # def after_sign_in_path_for(resource_or_scope)
-  #   if resource_or_scope.is_a?(User)
-  #     users_url
-  #   else
-  #     super
-  #   end
-  # end
-
-  # def after_sign_out_path_for(resource_or_scope)
-  #   #debugger
-  #   users_url
-  # end
 
   protected
 
